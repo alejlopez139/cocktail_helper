a950b98d12d00a51161516dc294a502c5eee0692
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-27 15:10:34 +0000

    finished ingredients

--- app.rb
+++ app.rb
@@ -18,8 +18,13 @@ end
 get("/ingredient_result")do
 @ingredient = params.fetch("ingredient").to_s
 url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{@ingredient}"
-conversion_data = JSON.parse(HTTP.get(url))
-@drinks = conversion_data['drinks']
-##need to output the key value of "strDrink"
+@conversion_data = JSON.parse(HTTP.get(url))
+@drinks = @conversion_data['drinks']
+
   erb(:ingredient_result)
+end
+
+get("/first_letter")do
+
+  erb(:first_letter)
 end
