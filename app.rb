3b855b78458ad818e519e80e0a6c92193c9e74da
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-09-27 16:58:55 +0000

    testing name search

--- app.rb
+++ app.rb
@@ -34,11 +34,44 @@ get("/first_letter_result")do
   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{@letter}"
   @conversion_data = JSON.parse(HTTP.get(url))
   @drinks = @conversion_data['drinks']
+  
 
   erb(:first_letter_result)
 end
 
 get("/random")do
+  url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  @conversion_data = JSON.parse(HTTP.get(url))
+  @drinks = @conversion_data['drinks']
+  @image = @drinks.first.fetch("strDrinkThumb").to_s
+  @image = @image + "/preview"
 
+  @ingredients = []
+  num_of_ingredients = 1
+  until num_of_ingredients == 15 do
+    @ingredients.push(@drinks.first.fetch("strIngredient#{num_of_ingredients}"))
+    num_of_ingredients = num_of_ingredients + 1
+  end
+  @measure = []
+  num_of_measurements = 1
+  until num_of_measurements == 15 do
+    @measure.push(@drinks.first.fetch("strMeasure#{num_of_measurements}"))
+    num_of_measurements = num_of_measurements + 1
+  end
+  @ingredients.compact!
+  @measure.compact!
   erb(:random)
+end
+
+get("/name") do
+  erb(:name)
+end
+
+get("/name_result") do
+  @drink_name = params.fetch("name").to_s
+  url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i={#@drink_name}"
+  @conversion_data = JSON.parse(HTTP.get(url))
+  @drinks = @conversion_data['drinks']
+  
+  erb(:name_result)
 end
